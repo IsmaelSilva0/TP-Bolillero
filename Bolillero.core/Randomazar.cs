19c52b6a55d3c9c7bf95c1f596126ef7f2eb81dd
IsmaelSilva0 <ismaelsmithsilva2003@gmail.com>
2023-04-20 15:40:59 -0300

    recreacion por 1000 vez de clases
    kkk

--- Bolillero.core/Randomazar.cs
+++ Bolillero.core/Randomazar.cs
@@ -1,5 +1,9 @@
 namespace Bolillero.core;
-
-public class Randomazar
+public class Randomazar : IRandom
 {
+    private Random _random { get; set; }
+    public Randomazar()
+        => _random = new Random(DateTime.Now.Millisecond);
+    public int SacarIndice(List<int> adentro)
+        => _random.Next(0, adentro.Count);
 }
