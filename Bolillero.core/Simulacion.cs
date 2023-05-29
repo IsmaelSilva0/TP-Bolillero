fb78022918ec0d1433b3d579dc1245c9e388101b
Ismael <=>
2023-05-29 17:20:08 -0300

    Simulaciones con hilos and sin hilos

--- Bolillero.core/Simulacion.cs
+++ Bolillero.core/Simulacion.cs
@@ -2,4 +2,11 @@ namespace Bolillero.core;
 
 public class Simulacion
 {
+    long simularSinHilos(Bolillero1 bolillero, List<int> jugada, int cantsimulaciones)
+
+    => bolillero.JugarNVeces(jugada, cantsimulaciones);
+    long simularConHilos(Bolillero1 bolillero1, List<int> jugada, int cantsimulaciones, int cantHilos)
+    {
+    Task<int>[] tareas = new Task<int>[cantHilos];
+    }
 }
