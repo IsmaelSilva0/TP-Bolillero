90db80b8e948676f8600b2c85f1e0b43c50d4bf6
Ismael <=>
2023-06-26 17:01:03 -0300

    corrigiendo la class Simulacion

--- Bolillero.core/Simulacion.cs
+++ Bolillero.core/Simulacion.cs
@@ -7,10 +7,11 @@ public class Simulacion
     => bolillero.JugarNVeces(jugada, cantsimulaciones);
     long simularConHilos(Bolillero1 bolillero, List<int> jugada, int cantsimulaciones, int cantHilos)
     {
-    Task<long>[] cantsimulaciones = new Task<long>[cantHilos];
+    Task<long>[] simulaciones = new Task<long>[cantHilos];
+    Array.ForEach(simulaciones, t => t.Start());
     for ( int i = 0; i <= cantHilos;i++)
         {
-
+            
         }
     }
 }
